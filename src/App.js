4bfde1cf007baad24d24048a8dc2392cabbc6536
Eric Zumwalt <ezumwalt@br2520.jmm.org>
2019-11-08 15:42:02 -0600

    Started item boxes

--- src/App.js
+++ src/App.js
@@ -18,6 +18,30 @@ function App() {
         </section>
 
         <section className="part-2">
+          <div className="item-box-container">
+
+            <div className="item-box">
+              <i className="fas fa-gift icon"></i>
+              <div className="item-box-desc">
+              <p>This is the first item box in this row.</p>
+              </div>
+            </div>
+
+            <div className="item-box">
+              <i className="fab fa-fort-awesome icon"></i>
+              <div className="item-box-desc">
+              <p>This is the second item box in this row.</p>
+              </div>
+            </div>
+
+            <div className="item-box">
+              <i className="fab fa-odnoklassniki icon"></i>
+              <div className="item-box-desc">
+              <p>This is the third item box in this row.</p>
+              </div>
+            </div>
+
+          </div>
         </section>
       </main>
     </div>
