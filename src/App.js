4cb5b833beafaace384924d80a9717c254747bfb
Eric Zumwalt <ezumwalt@CS1303.local>
2019-11-08 13:07:42 -0600

    Update favicon, appleicon, and manifest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React2
-        </a>
+      <header className="header">
+        <p>This is a Header</p>
       </header>
+      <main className="main">
+        <section className="top-section-container">
+          <h1 className="main-title">Don't call it a Side Proj!</h1>
+        </section>
+      </main>
     </div>
   );
 }
