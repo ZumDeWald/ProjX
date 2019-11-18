028e8d4876da1ff396cd607cc1543d7dca353b51
Eric Zumwalt <ezumwalt@CS1303.local>
2019-11-18 14:15:39 -0600

    Added Keen theme scripts and begin testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './FontAwesome.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
