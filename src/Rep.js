dd14442c7cbbbef14752e3add8b62921e9d71dda
Eric Zumwalt <ezumwalt@CS1303.local>
2019-11-26 12:25:36 -0600

    More work done on Rep view

--- src/Rep.js
+++ src/Rep.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FormResponses, {SampleEmailQuestions} from './ExampleEmailData.js';
 import './rep.css';
 
 function Rep() {
+
+  const [currentEmail, setCurrentEmail] = useState(SampleEmailQuestions.first);
+
+  const appendTextArea = (insertText) => {
+    document.getElementById("response-textarea").value += `${insertText}`;
+  }
 
   return (
     <main className="main">
@@ -13,9 +20,14 @@ function Rep() {
       </section>
       <section className="main-center">
         <div className="rep-text-area-container">
-          <textarea rows="10" className="rep-text-area">
-            ENTER TEXT HERE
+          <textarea id="response-textarea" rows="10" className="rep-text-area">
+            {`${currentEmail}
+
+            `}
           </textarea>
+            {FormResponses.map((item, i) => (
+              <button value={item.title} key={`form ${i}`} onClick={() => {appendTextArea(FormResponses[i].response)}}>{item.title}</button>
+            ))}
         </div>
       </section>
     </main>
