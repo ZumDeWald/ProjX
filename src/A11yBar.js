23128455791f29dd1ef1129fbc47db9e69214e3c
Eric Zumwalt <ezumwalt@CS1303.local>
2019-11-27 12:29:01 -0600

    Light and dark mode changes

--- src/A11yBar.js
+++ src/A11yBar.js
@@ -5,26 +5,34 @@ import './a11yBar.css';
 
 const A11yBar = () => {
 
-  const [fontSize, setFontSize] = useState("standard");
-
   const root = document.documentElement;
+  const [fontSize, setFontSize] = useState("standard");
+  const [theme, setTheme] = useState("light");
 
   const changeSize = () => {
     if (fontSize === "standard") {
       setFontSize("large");
       root.style.setProperty("--variable-font", "var(--font-large)");
-      console.log(fontSize);
     } else if (fontSize === "large") {
       setFontSize("standard");
       root.style.setProperty("--variable-font", "var(--font-med)");
-      console.log(fontSize);
+    }
+  }
+
+  const changeTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      root.style.setProperty("--background", "var(--theme-dark)");
+      root.style.setProperty("--foreground", "var(--theme-light)");
+    } else if (theme === "dark") {
+      setTheme("light");
+      root.style.setProperty("--background", "var(--theme-light)");
+      root.style.setProperty("--foreground", "var(--theme-dark)");
     }
   }
 
   const a11yContainer = document.getElementsByClassName("a-bar-outer-container");
-
   const a11yIconContainer = document.getElementsByClassName("a11y-icon-container");
-
   const a11yIcon = document.getElementsByClassName("a11y-icon");
 
   const openCloseA11y = () => {
@@ -49,7 +57,7 @@ const A11yBar = () => {
         <div className="a11y-option-container grid-position-1">
           <h5 className="a11y-option-title">Light/Dark Mode</h5>
           <div className="option-slider-container">
-            <div className="option-slider-button" onClick={buttonSlide}></div>
+            <div className="option-slider-button" onClick={(e) => {buttonSlide(e); changeTheme()}}></div>
           </div>
         </div>
 
